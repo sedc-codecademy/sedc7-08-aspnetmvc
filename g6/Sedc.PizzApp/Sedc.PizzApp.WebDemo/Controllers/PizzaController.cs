414fc714466b3e01ee5a2401cf358516001b3641
xhevo <dzevat.ibraimi@navetti.com>
2019-06-01 11:55:35 +0200

    added pizza controller

---
+++ g6/Sedc.PizzApp/Sedc.PizzApp.WebDemo/Controllers/PizzaController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Sedc.PizzApp.WebDemo.Controllers
+{
+    public class PizzaController : Controller
+    {
+        public IActionResult GetAll()
+        {
+            ViewBag.Pizzas = new List<string>
+            {
+                "capri", "tuna","margarita","pepperoni"
+            };
+            return View();
+        }
+    }
+}
