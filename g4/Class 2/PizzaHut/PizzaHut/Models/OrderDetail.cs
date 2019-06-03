aad28d5351571bae6e9767c145a445ffeb6d16e9
Igor Micev <igormicev@gmail.com>
2019-06-03 21:53:08 +0200

    Class 2

---
+++ g4/Class 2/PizzaHut/PizzaHut/Models/OrderDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaHut.Models
+{
+    public class OrderDetail : IEntity
+    {
+        public int ID { get; set; }
+        public int OrderID { get; set; }
+        public int PizzaID { get; set; }
+        public decimal UnitPrice { get; set; }
+        public short Quantity { get; set; }
+        public decimal Discount { get; set; }
+
+        public virtual Order Order { get; set; }
+        public virtual Pizza Pizza { get; set; }
+    }
+}
---
+++ g4/Class 2/PizzaHut/PizzaHut/Models/PizzaType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaHut.Models
+{
+    public class PizzaType : IEntity
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Photo { get; set; }
+
+        public virtual ICollection<Pizza> Pizzas { get; set; }
+    }
+}
