020e915bb62a72026465f4fbcc431d7631cd74d0
xhevo <dzevat.ibraimi@navetti.com>
2019-06-05 20:48:14 +0200

    added extra properties in pizza

--- g6/Sedc.PizzApp/Sedc.PizzApp.WebDemo/Models/Pizza.cs
+++ g6/Sedc.PizzApp/Sedc.PizzApp.WebDemo/Models/Pizza.cs
@@ -4,5 +4,8 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int Size { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
     }
 }
