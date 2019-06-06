5061ee36e2503a51796e37cd8b4c1f68f03ab445
xhevo <dzevat.ibraimi@navetti.com>
2019-06-06 17:50:25 +0200

    added class3 copy

---
+++ g1/Class 3/code/Sedc.PizzApp.WebDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sedc.PizzApp.WebDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
