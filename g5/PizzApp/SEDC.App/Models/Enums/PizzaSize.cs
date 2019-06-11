faebcea906dc833eac87498825ac6fff39588f9e
dragan <dragang@iborn.net>
2019-06-11 19:58:09 +0200

    Update on code Class 5

---
+++ g5/PizzApp/SEDC.App/Models/Enums/PizzaSize.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.App.Models.Enums
+{
+    public enum PizzaSize
+    {
+        Medium = 1,
+        Large = 2,
+        Family = 3
+    }
+}
---
+++ g5/PizzApp/SEDC.App/Models/ViewModels/OrderViewModel.cs
@@ -0,0 +1,18 @@
+﻿using SEDC.App.Models.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.App.Models.ViewModels
+{
+    public class OrderViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public long Phone { get; set; }
+        public string Pizza { get; set; }
+        public PizzaSize Size { get; set; }
+    }
+}
