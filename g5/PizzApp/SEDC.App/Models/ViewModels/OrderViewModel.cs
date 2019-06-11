f78db89bca7a51de96024690a7c6a1b3fa3549d0
dragan <dragang@iborn.net>
2019-06-11 20:57:41 +0200

    Class 5 final code

--- g5/PizzApp/SEDC.App/Models/ViewModels/OrderViewModel.cs
+++ g5/PizzApp/SEDC.App/Models/ViewModels/OrderViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using SEDC.App.Models.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,14 @@ namespace SEDC.App.Models.ViewModels
 {
     public class OrderViewModel
     {
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Display(Name = "Address for delivery")]
         public string Address { get; set; }
         public long Phone { get; set; }
+        [Display(Name = "Type of pizza")]
         public string Pizza { get; set; }
         public PizzaSize Size { get; set; }
     }
