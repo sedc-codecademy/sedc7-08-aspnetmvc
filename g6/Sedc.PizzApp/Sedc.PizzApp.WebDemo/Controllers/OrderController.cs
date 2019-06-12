5c01bbbeec9491b9d42b3fe0e0f0f340dc1c23a7
xhevo <dzevat.ibraimi@navetti.com>
2019-06-12 18:53:21 +0200

    added custom routing and route constraints

---
+++ g6/Sedc.PizzApp/Sedc.PizzApp.WebDemo/Controllers/OrderController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sedc.PizzApp.WebDemo.Controllers
+{
+
+    public class OrderController : Controller
+    {
+        [Route("/user/{userId:range(1,2000000000)}/pizza/{pizzaId:int}/order/{orderId}")]
+        //[ActionName("history")]
+        public IActionResult GetUserPizzaOrder
+            (int userId, int pizzaId, string orderId)
+        {
+            var pizzas = new string[] {
+                "margharita",
+                "pepperoni",
+                "capri"
+            };
+            return new OkObjectResult(pizzas);
+        }
+    }
+}
