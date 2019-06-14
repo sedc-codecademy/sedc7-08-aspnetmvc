74def52e03df91fa5771cf8d8ee6bc63308339c9
Risto Pavlov <ristopavlov@gmail.com>
2019-06-14 23:16:36 +0200

    added UI for handling the PIzza, Size and Price for each pizza size combination

---
+++ g2/Sedc.G2.PizzApp/Sedc.G2.PizzApp/Models/PizzaPriceListViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Sedc.G2.PizzApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sedc.G2.PizzApp.Models
+{
+    public class PizzaPriceListViewModel
+    {
+        public List<Model.PizzaSizeModel> PizzaPrices { get; set; }
+
+        public List<Model.PizzaModel> Pizzas { get; set; }
+
+        public List<Model.SizeModel> Sizes { get; set; }
+    }
+}
