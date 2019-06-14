32f91ee39a7b9712bd8a9eb1c1597f38e02a8001
Risto Pavlov <ristopavlov@gmail.com>
2019-06-14 22:05:10 +0200

    added new project for PizzApp models and created the data model for our application

---
+++ g2/Sedc.G2.PizzApp/Sedc.G2.PizzApp.Model/OrderModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sedc.G2.PizzApp.Model
+{
+    public class OrderModel
+    {
+        public int Id { get; set; }
+        public bool ToBeDelivered { get; set; }
+
+        public int UserId { get; set; }
+    }
+}
---
+++ g2/Sedc.G2.PizzApp/Sedc.G2.PizzApp.Model/SizeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sedc.G2.PizzApp.Model
+{
+    public class SizeModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Dimension { get; set; }
+
+    }
+}
