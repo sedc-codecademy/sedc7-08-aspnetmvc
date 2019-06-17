27fd2bc7d6a60ddcd7bd089a6cb153abd5af5078
xhevo <dzevat.ibraimi@navetti.com>
2019-06-17 20:29:06 +0200

    added scaffolding

--- g6/Sedc.PizzApp/Sedc.PizzApp.WebDemo/Models/Drink.cs
+++ g6/Sedc.PizzApp/Sedc.PizzApp.WebDemo/Models/Drink.cs
@@ -1,11 +1,9 @@
 ﻿namespace Sedc.PizzApp.WebDemo.Models
 {
-    public class Pizza
-    {   
+    public class Drink
+    {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Size { get; set; }
         public double Price { get; set; }
-        public string Description { get; set; }
     }
 }
---
+++ g6/Sedc.PizzApp/Sedc.PizzApp.WebDemo/Models/OrderItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Sedc.PizzApp.WebDemo.Models
+{
+    public class OrderItem
+    {
+        public int PizzaId { get; set; }
+        public int OrderId { get; set; }
+        public int Quatity { get; set; }
+    }
+}
