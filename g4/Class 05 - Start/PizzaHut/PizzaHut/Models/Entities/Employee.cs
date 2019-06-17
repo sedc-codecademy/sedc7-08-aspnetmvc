dddecaf4361ef619281f1fd58e4f4ef10c7ddc59
Igor Micev <igormicev@gmail.com>
2019-06-17 12:24:36 +0200

    Class 05 Start

---
+++ g4/Class 05 - Start/PizzaHut/PizzaHut/Models/Entities/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaHut.Models
+{
+    public class Employee : IEntity
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime HireDate { get; set; }
+        public string City { get; set; }
+
+        public virtual ICollection<Order> Orders { get; set; }
+
+    }
+}
