8a3c7ab224c5e769f839eb98598c7bf6685d6088
Martin <panovski62@gmail.com>
2019-06-19 21:14:05 +0200

    Class 6 added

---
+++ g2/Class 6/PizzaApp - Refactored/SEDC.PizzaApp/SEDC.PizzaApp.Domain/Pizza.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEDC.PizzaApp.Domain
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public PizzaSize Size { get; set; }
+        public int Price { get; set; }
+    }
+}
