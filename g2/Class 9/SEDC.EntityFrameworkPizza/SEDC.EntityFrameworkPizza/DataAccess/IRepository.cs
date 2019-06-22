7c25ccecd3935d4a44f55842b0ea0ebbba326ffc
Martin <panovski62@gmail.com>
2019-06-22 16:14:55 +0200

    Class 8 - Added entity framework demo app

---
+++ g2/Class 9/SEDC.EntityFrameworkPizza/SEDC.EntityFrameworkPizza/DataAccess/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.EntityFrameworkPizza.DataAccess
+{
+    public interface IRepository<T> where T : class
+    {
+        List<T> GetAll();
+        T GetById(int id);
+        void Create(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
