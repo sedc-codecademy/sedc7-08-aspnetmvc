f0d2c80d4bd1852de63fe6c207600606ab4b7714
Dejan <djovanov92@gmail.com>
2019-06-22 20:33:14 +0200

    Updated folder name and added Homework

---
+++ g2/Class 8/SEDC.EntityFrameworkPizza/SEDC.EntityFrameworkPizza/DataAccess/Domain/Order.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SEDC.EntityFrameworkPizza.DataAccess.Domain
+{
+    public class Order
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+        public List<Pizza> Pizzas { get; set; }
+
+    }
+}
