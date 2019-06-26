029a02063ba17826cffe91dbbc9060c959ac62d3
xhevo <dzevat.ibraimi@navetti.com>
2019-06-26 19:38:46 +0200

    added database context

--- g6/Sedc.PizzApp/PizzApp.Repositories.EntityFramework/ApplicationDatabase.cs
+++ g6/Sedc.PizzApp/PizzApp.Repositories.EntityFramework/ApplicationDatabase.cs
@@ -1,8 +1,20 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using PizzApp.Models;
 
 namespace PizzApp.Repositories.EntityFramework
 {
     public class ApplicationDatabase: DbContext
     {
+        public ApplicationDatabase(
+            DbContextOptions<ApplicationDatabase> options)
+            :base(options)
+        {
+            //0Database.EnsureCreated();
+            //base.Database.BeginTransaction();
+            //base.Database.RollbackTransaction();
+            //base.Database.CommitTransaction();
+        }
+        public DbSet<Pizza> Pizzas { get; set; }
+
     }
 }
--- g6/Sedc.PizzApp/PizzApp.Repositories.EntityFramework/EntityFrameworkPizzaRepository.cs
+++ g6/Sedc.PizzApp/PizzApp.Repositories.EntityFramework/EntityFrameworkPizzaRepository.cs
@@ -1,12 +1,22 @@
-﻿using PizzApp.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using PizzApp.Models;
 using PizzApp.Repositories.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace PizzApp.Repositories.EntityFramework
 {
     public class EntityFrameworkPizzaRepository : IPizzaRepository
     {
+        private readonly ApplicationDatabase Database;
+
+        public EntityFrameworkPizzaRepository()
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDatabase>();
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PizzaDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            Database = new ApplicationDatabase(optionsBuilder.Options);
+        }
         public Pizza Create(Pizza model)
         {
             throw new NotImplementedException();
@@ -19,12 +29,13 @@ namespace PizzApp.Repositories.EntityFramework
 
         public List<Pizza> GetAllPizzas()
         {
-            throw new NotImplementedException();
+            return Database.Pizzas.ToList();
         }
 
         public Pizza GetById(int id)
         {
-            throw new NotImplementedException();
+            return Database.Pizzas
+                .FirstOrDefault(pizza => pizza.Id == id);
         }
 
         public Pizza Update(Pizza model)
