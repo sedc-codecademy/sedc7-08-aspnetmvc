ec9dff79a3f36a4b701a1889471391b9a0c51c9f
xhevo <dzevat.ibraimi@navetti.com>
2019-06-29 14:22:46 +0200

    added parametrization

--- g1/ado.net/AdoNetDemo/Program.cs
+++ g1/ado.net/AdoNetDemo/Program.cs
@@ -8,13 +8,32 @@ namespace ado.net
         static void Main(string[] args)
         {
             //executeScalar() - get a number
-            Console.WriteLine(GetNumberOfPizzas());
-            
+            //Console.WriteLine(GetNumberOfPizzas());
+
             //executeNonQuery() - get number of rows affected
-            Console.WriteLine(UpdatePizza(1, "amerikana"));
+            Console.WriteLine("enter pizza id:");
+            var pizzaId = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter pizza name:");
+            var pizzaName = Console.ReadLine();
+            Console.WriteLine("Rows affected: " + UpdatePizza(pizzaId, pizzaName));
         }
 
-        
+        public static int UpdatePizza(int pizzaId, string pizzaName)
+        {
+            var connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PizzaDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            SqlConnection sqlConnection = new SqlConnection(connStr);
+            sqlConnection.Open();
+            var query = $"UPDATE Pizzas SET Name=@pizzaName where Id = @pizzaId";
+
+            SqlCommand sqlCommand = new SqlCommand(query);
+            sqlCommand.Parameters.AddWithValue("pizzaName", pizzaName);
+            sqlCommand.Parameters.AddWithValue("pizzaId", pizzaId);
+            sqlCommand.Connection = sqlConnection;
+            var numberOfPizzas = sqlCommand.ExecuteNonQuery();
+            sqlConnection.Close();
+            return (int)numberOfPizzas;
+        }
+
 
         public static int GetNumberOfPizzas()
         {
@@ -27,5 +46,7 @@ namespace ado.net
             sqlConnection.Close();
             return (int)numberOfPizzas;
         }
+
+
     }
 }
