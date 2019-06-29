9e9bb717239d4b4bf2d8f824b6c0491cd91a134e
Stojancho Jefremov <stojancho.jefremov@seavus.com>
2019-06-29 14:19:17 +0200

    [G4] - Class 9 added, not finished yet

---
+++ g4/Class 9/ToDo/ToDo.WebApp/Controllers/TasksController.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using ToDo.WebApp.Models;
+//using todoModels = ToDo.WebApp.Models;
+using ToDo.WebApp.Models.Enums;
+using ToDo.WebApp.ViewModels;
+//using System.Threading.Tasks;
+
+namespace ToDo.WebApp.Controllers
+{
+    public class TasksController : Controller
+    {
+        private static IList<Task> _tasks;
+
+        static TasksController()
+        {
+            InitializeTasks();
+        }
+
+        private static void InitializeTasks()
+        {
+            _tasks = new List<Task>
+            {
+                new Task
+                {
+                    Description = "Task 1 description",
+                    Priority = Priority.Important,
+                    Status = Status.ToDo,
+                    Title = "Task 1 title",
+                    Type = TaskType.Work,
+                    SubTasks = new List<SubTask>
+                    {
+                        new SubTask
+                        {
+                            Description = "SubTask 1, Task 1 description",
+                            Status = Status.ToDo,
+                            Title = "SubTask 1, Task 1 title"
+                        },
+                        new SubTask
+                        {
+                            Description = "SubTask 2, Task 1 description",
+                            Status = Status.Done,
+                            Title = "SubTask 2, Task 1 title"
+                        },
+                        new SubTask
+                        {
+                            Description = "SubTask 3, Task 1 description",
+                            Status = Status.InProgress,
+                            Title = "SubTask 3, Task 1 title"
+                        }
+                    }
+                },
+                new Task
+                {
+                    Description = "Task 2 description",
+                    Priority = Priority.Medium,
+                    Status = Status.InProgress,
+                    Title = "Task 2 title",
+                    Type = TaskType.Hobby,
+                    SubTasks = new List<SubTask>
+                    {
+                        new SubTask
+                        {
+                            Description = "SubTask 1, Task 2 description",
+                            Status = Status.InProgress,
+                            Title = "SubTask 1, Task 2 title"
+                        }
+                    }
+                },
+                new Task
+                {
+                    Description = "Task 3 description",
+                    Priority = Priority.NotImportant,
+                    Status = Status.Done,
+                    Title = "Task 3 title",
+                    Type = TaskType.Personal,
+                    SubTasks = new List<SubTask>
+                    {
+                        new SubTask
+                        {
+                            Description = "SubTask 1, Task 3 description",
+                            Status = Status.Done,
+                            Title = "SubTask 1, Task 3 title"
+                        },
+                        new SubTask
+                        {
+                            Description = "SubTask 2, Task 3 description",
+                            Status = Status.Done,
+                            Title = "SubTask 2, Task 3 title"
+                        }
+                    }
+                },
+
+            };
+
+            foreach (var task in _tasks)
+            {
+                foreach (var subTask in task.SubTasks)
+                {
+                    subTask.ParentTask = task;
+                }
+            }
+        }
+
+        public IActionResult Index(Status? status)
+        {
+            IEnumerable<Task> filteredTasks;
+            if (status == null)
+                filteredTasks = _tasks;
+            else
+                filteredTasks = _tasks.Where(t => t.Status == status);
+
+            Array enumValues = Enum.GetValues(typeof(Status));
+            var statusListItems = new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Text = " - All - ",
+                    Value = string.Empty,
+                    Selected = !status.HasValue
+                }
+            };
+            foreach (object enumValue in enumValues)
+            {
+                var selectListItem = new SelectListItem
+                {
+                    Value = enumValue.ToString(),
+                    Text = Enum.GetName(typeof(Status), enumValue),
+                    Selected = status.HasValue 
+                                        && (int)enumValue == (int)status
+                };
+
+                statusListItems.Add(selectListItem);
+            }
+
+            var viewModel = new TasksViewModel
+            {
+                Tasks = filteredTasks,
+                Statuses = statusListItems
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(Task task)
+        {
+            if (ModelState.IsValid)
+            {
+                _tasks.Add(task);
+
+                return RedirectToAction("ToDo");
+            }
+
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            var task = _tasks.SingleOrDefault(t => t.Id == id);
+            if (task == null)
+                return NotFound($"Task with id: {id} was not found");
+
+            return View(task);
+        }
+    }
+}
