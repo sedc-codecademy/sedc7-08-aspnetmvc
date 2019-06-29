4d14f8d202d47e59eedf7adeb35161f28cab2528
xhevo <dzevat.ibraimi@navetti.com>
2019-06-29 14:00:47 +0200

    added executescalar

--- g1/ado.net/AdoNetDemo/Program.cs
+++ g1/ado.net/AdoNetDemo/Program.cs
@@ -7,26 +7,25 @@ namespace ado.net
     {
         static void Main(string[] args)
         {
+            //executeScalar() - get a number
+            Console.WriteLine(GetNumberOfPizzas());
+            
+            //executeNonQuery() - get number of rows affected
+            Console.WriteLine(UpdatePizza(1, "amerikana"));
+        }
+
+        
+
+        public static int GetNumberOfPizzas()
+        {
             var connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=PizzaDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
             SqlConnection sqlConnection = new SqlConnection(connStr);
             sqlConnection.Open();
-            //
-            SqlCommand sqlCommand = new SqlCommand();
-            sqlCommand.CommandText = "select * from Pizzas";
-            
-            //get number of pizzas
-            object scalarResult = sqlCommand.ExecuteScalar();
-
-
-            //write queries, update pizza name
-            int rowsAffected = sqlCommand.ExecuteNonQuery();
-
-
-            //
+            SqlCommand sqlCommand = new SqlCommand("select count(*) from Pizzas");
+            sqlCommand.Connection = sqlConnection;
+            var numberOfPizzas = sqlCommand.ExecuteScalar();
             sqlConnection.Close();
-
-
-
+            return (int)numberOfPizzas;
         }
     }
 }
