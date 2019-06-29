26aaa5a8f988dc203e54f5ac3ebd6285596aafaa
Stojancho Jefremov <stojancho.jefremov@seavus.com>
2019-06-29 16:07:58 +0200

    [G4] - Class 9 final add

---
+++ g4/Class 9/ToDo/ToDo.WebApp.UnitTests/TasksDbTests.cs
@@ -0,0 +1,36 @@
+using NUnit.Framework;
+using ToDo.WebApp.Controllers;
+
+namespace Tests
+{
+    public class TasksDbTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            //// Arrange
+            ///
+            ////var mockRepo = new Mock<IBrainstormSessionRepository>();
+            ////mockRepo.Setup(repo => repo.ListAsync())
+            ////    .ReturnsAsync(GetTestSessions());
+            //var controller = new TasksDbController(new ToDo.WebApp.TasksDbContext()))
+            //    new ToDo.WebApp.TasksDbContext());
+
+            //// Act
+
+            //var result = await controller.Index();
+
+            //// Assert
+
+            //var viewResult = Assert.IsType<ViewResult>(result);
+            //var model = Assert.IsAssignableFrom<IEnumerable<StormSessionViewModel>>(
+            //    viewResult.ViewData.Model);
+            //Assert.Equal(2, model.Count());
+        }
+    }
+}
---
+++ g4/Class 9/ToDo/ToDo.WebApp/TasksDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDo.WebApp.Models;
+
+namespace ToDo.WebApp
+{
+    public class TasksDbContext : DbContext
+    {
+        public TasksDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Task> Tasks { get; set; }
+    }
+}
