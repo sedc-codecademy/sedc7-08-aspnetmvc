dc3750b3bb4486e4ad435c8171be2284d1717888
xhevo <dzevat.ibraimi@navetti.com>
2019-07-01 20:04:15 +0200

    added data reader

--- g6/ado.net/AdoNetDemo/Program.cs
+++ g6/ado.net/AdoNetDemo/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using AdoNetDemo;
+using System;
+using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace ado.net
 {
@@ -11,16 +14,48 @@ namespace ado.net
         static void Main(string[] args)
         {
             connection.Open();
+            //execute scalar
             //var numberOfPizzas = GetNumberOfPizzas();
             //Console.WriteLine(numberOfPizzas);
-            Console.Write("id: ");
-            int id = int.Parse(Console.ReadLine());
-            Console.WriteLine();
-            Console.Write("description: ");
-            string description = Console.ReadLine();
-            var rowsAffected = UpdatePizzaDescription(id, description);
-            Console.WriteLine(rowsAffected);
+
+            //execute non query and sql injection
+            //Console.Write("id: ");
+            //int id = int.Parse(Console.ReadLine());
+            //Console.WriteLine();
+            //Console.Write("description: ");
+            //string description = Console.ReadLine();
+            //var rowsAffected = UpdatePizzaDescription(id, description);
+            //Console.WriteLine(rowsAffected);
+
+            //execute reader
+            IEnumerable<Pizza> pizzas = GetPizzasFromDatabase();
+            foreach (var pizza in pizzas)
+            {
+                Console.WriteLine($"#{pizza.Id}, {pizza.Name} '{pizza.Description}'");
+            }
+
             connection.Close();
+        }
+
+        private static IEnumerable<Pizza> GetPizzasFromDatabase()
+        {
+            string query = "select * from Pizzas";
+            var command = new SqlCommand(query, connection);
+
+            var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var id = (int)reader["Id"];
+                string name = (string)reader["Name"];
+                string desctiption = reader["Description"] as string ?? "DBNULL";
+                var pizza = new Pizza
+                {
+                    Id = id,
+                    Description = desctiption,
+                    Name = name
+                };
+                yield return pizza;
+            }
         }
 
         static int UpdatePizzaDescription(int id, string description)
@@ -41,5 +76,6 @@ namespace ado.net
             var numberOfPizzas = (int)command.ExecuteScalar();
             return numberOfPizzas;
         }
+
     }
 }
