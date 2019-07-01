ce0ef783cfb975965f52e74fbdbe1452edc7ab09
xhevo <dzevat.ibraimi@navetti.com>
2019-07-01 18:31:54 +0200

    added adonet scalar

--- g6/ado.net/AdoNetDemo/Program.cs
+++ g6/ado.net/AdoNetDemo/Program.cs
@@ -1,9 +1,22 @@
-﻿namespace ado.net
+﻿using System;
+using System.Data.SqlClient;
+
+namespace ado.net
 {
     class Program
     {
+        private const string connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TestPizzaDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         static void Main(string[] args)
         {
+            var connection = new SqlConnection(connStr);
+            connection.Open();
+            //////
+            string query = "select count(*) from Pizzas";
+            var command = new SqlCommand(query, connection);
+            var numberOfPizzas = (int)command.ExecuteScalar();
+            Console.WriteLine(numberOfPizzas);
+            /////
+            connection.Close();
         }
     }
 }
