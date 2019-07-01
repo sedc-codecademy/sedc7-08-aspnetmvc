a448505dea8e91f707a0503f695f679af14a54ec
xhevo <dzevat.ibraimi@navetti.com>
2019-07-01 19:19:43 +0200

    added parametrisation

--- g6/ado.net/AdoNetDemo/Program.cs
+++ g6/ado.net/AdoNetDemo/Program.cs
@@ -6,17 +6,40 @@ namespace ado.net
     class Program
     {
         private const string connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=TestPizzaDatabase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private static SqlConnection connection = new SqlConnection(connStr);
+
         static void Main(string[] args)
         {
-            var connection = new SqlConnection(connStr);
             connection.Open();
-            //////
+            //var numberOfPizzas = GetNumberOfPizzas();
+            //Console.WriteLine(numberOfPizzas);
+            Console.Write("id: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("description: ");
+            string description = Console.ReadLine();
+            var rowsAffected = UpdatePizzaDescription(id, description);
+            Console.WriteLine(rowsAffected);
+            connection.Close();
+        }
+
+        static int UpdatePizzaDescription(int id, string description)
+        {
+            string query = $"update Pizzas set Description = @description where Id = @id";
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("description", description);
+            command.Parameters.AddWithValue("id", id);
+
+            var rowsAffected = command.ExecuteNonQuery();
+            return rowsAffected;
+        }
+
+        static int GetNumberOfPizzas()
+        {
             string query = "select count(*) from Pizzas";
             var command = new SqlCommand(query, connection);
             var numberOfPizzas = (int)command.ExecuteScalar();
-            Console.WriteLine(numberOfPizzas);
-            /////
-            connection.Close();
+            return numberOfPizzas;
         }
     }
 }
