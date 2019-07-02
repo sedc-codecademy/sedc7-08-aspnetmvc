9c2a2c0fc32400329f202dabb81c01f72996eebc
xhevo <dzevat.ibraimi@navetti.com>
2019-07-02 17:55:21 +0200

    fixed errors

--- g1/Source Code/Sedc.Todo03Solution.Repositories/TodoContextFactory.cs
+++ g1/Source Code/Sedc.Todo03Solution.Repositories/TodoContextFactory.cs
@@ -1,16 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Design;
+﻿//using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore.Design;
 
-namespace Sedc.Todo03Solution.Repositories
-{
-    class TodoContextFactory: IDesignTimeDbContextFactory<TodoContext>
-    {
-        public TodoContext CreateDbContext(string[] args)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<TodoContext>();
-            optionsBuilder.UseInMemoryDatabase("TodoApp");
+//namespace Sedc.Todo03Solution.Repositories
+//{
+//    class TodoContextFactory: IDesignTimeDbContextFactory<TodoContext>
+//    {
+//        public TodoContext CreateDbContext(string[] args)
+//        {
+//            var optionsBuilder = new DbContextOptionsBuilder<TodoContext>();
+//            optionsBuilder.UseInMemoryDatabase("TodoApp");
 
-            return new TodoContext(optionsBuilder.Options);
-        }
-    }
-}
+//            return new TodoContext(optionsBuilder.Options);
+//        }
+//    }
+//}
